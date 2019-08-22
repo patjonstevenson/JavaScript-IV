c0373e8d14d9dfc1f4d928ef7c9acab6123eda5e
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-08-22 10:59:39 -0700

    started prototype-refactor.js, added instructions for lambda-classes.js

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,13 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+class GameObject {
+  constructor(obj) {
+    this.createdAt = obj.createdAt;
+    this.name = obj.name;
+    this.dimensions = obj.dimensions;
+  }
+  destroy() {
+    console.log(`${this.name} was removed from the game.`);
+  }
+}
